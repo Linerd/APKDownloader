769be52d3378a13418644336ee3e81742707f9a6
Yikai <yklin@eecs.umich.edu>
2016-01-17 16:06:13 -0500

    updated download.py

--- download.py
+++ download.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 import os
 import time
+import sys
 
-with open('app.url', 'r') as r:
+filename = sys.argv[1]
+
+with open(filename+'.txt', 'r') as r:
 	for line in r:
-		os.system("cd /z/apks/Downloads/app/ && wget --recursive --referer=https://apkpure.com/app?sort=download %s" %line.strip('\n'))
-with open('game.url', 'r') as r:
-	for line in r:
-		os.system("cd /z/apks/Downloads/game/ && wget --recursive --referer=https://apkpure.com/app?sort=download %s" %line.strip('\n'))
+		os.system("wget -P /z/apks/"+filename+"/ %s" %line.strip('\n'))
+		time.sleep(5)
 		
